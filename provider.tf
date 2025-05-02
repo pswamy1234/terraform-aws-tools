a278c981f7acbe3f729adeb4ee312fd691627624
pswamy1234 <puttiswamy123@gmail.com>
2025-05-02 18:04:46 +0530

    Jenkins infra

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "swamy-jenkins"
-    key            = "jenkins-test-1"
+    key            = "jenkins-nexus-test1"
     region         = "us-east-1"
     dynamodb_table = "swamy-locking"
   }
