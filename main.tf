b962fb046bb430f05e734dbe6b7dfe612e9aea13
pswamy1234 <puttiswamy123@gmail.com>
2025-05-04 17:35:13 +0530

    expense infra

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+resource "aws_key_pair" "Swamy" {
+  key_name   = "Swamy"
+  # you can paste the public key directly like this
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZBLPI46+8kIFDPuE3UbvcWwKA6GIu0n8d8E1nsmDFKjxwWyF9P9I+dCz3QgmCT7+1PgdTVOqiEVwyhJ9kBFJ+fwmWPQw3VbzErqr7sEXvfgriG7kIwEj8+44lfOzTDL1bRUKG+Pe5Xd+mJF6pcBNtZmlx1x1ksYe7s1NtZbh+qxvTBk5WA7IPkHDmvc5pdNKx16SYiw1D62eH6fZa2jVrmNKcyT6+1grqBLL+bHyjYvafBPjVJ1EROhatWqhjxbdDoYb7z26ZkrmQys1zud2hbCSs9iflbNF4BvxD/L98Qmua18g6dHJfayk0p5hoOOHfLiyFsKPTPbTgW+zen6mRDQ1NJqBGoIll58GT2gyoXVgZ+HsP9Dw5deesdJ+0/hfYe/NxevAIR+H9CC+XFuwcpM0wZVRWEV3y6LWS4hZhI7AQrqVonZYeFdaPBdnXLdgwGl2+LH6lg08z+yKItOMQ2AjVVxpgMX/DNMPS5KXMj/hWvmHgtEInXrsBthk/iTDiMpHeT8Yv184Q9qf8yGpPipXLTA3mazYnVtZIrMhb3ISihDmblZF8+aZpibRp1N48Wo7+SPdoZJ+S2PYW2GVAhgdoIwzTYxARGh49PkYh4SLFJbVp9mQmWkKMil4CQQFfF9KSlM5mrEcZSDJuSQpA7TLMDQQ0uboxkSqMYjUlsQ== SAMSUNG@Swamy"
+  #public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
 module "jenkins" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
@@ -7,6 +15,7 @@ module "jenkins" {
   vpc_security_group_ids = ["sg-03931b1bad72d1a95"] #replace your SG
   subnet_id = "subnet-02785d205e5166b95" #replace your Subnet
   ami = data.aws_ami.ami_info.id
+  key_name = aws_key_pair.Swamy.key_name
   user_data = file("jenkins.sh")
   tags = {
     Name = "jenkins-tf"
@@ -23,19 +32,14 @@ module "jenkins_agent" {
   # convert StringList to list and get first element
   subnet_id = "subnet-06019b3c21801bd27"
   ami = data.aws_ami.ami_info.id
+  key_name = aws_key_pair.Swamy.key_name
   user_data = file("jenkins-agent.sh")
   tags = {
     Name = "jenkins-agent"
   }
 }
 
-resource "aws_key_pair" "Swamy" {
-  key_name   = "Swamy"
-  # you can paste the public key directly like this
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZBLPI46+8kIFDPuE3UbvcWwKA6GIu0n8d8E1nsmDFKjxwWyF9P9I+dCz3QgmCT7+1PgdTVOqiEVwyhJ9kBFJ+fwmWPQw3VbzErqr7sEXvfgriG7kIwEj8+44lfOzTDL1bRUKG+Pe5Xd+mJF6pcBNtZmlx1x1ksYe7s1NtZbh+qxvTBk5WA7IPkHDmvc5pdNKx16SYiw1D62eH6fZa2jVrmNKcyT6+1grqBLL+bHyjYvafBPjVJ1EROhatWqhjxbdDoYb7z26ZkrmQys1zud2hbCSs9iflbNF4BvxD/L98Qmua18g6dHJfayk0p5hoOOHfLiyFsKPTPbTgW+zen6mRDQ1NJqBGoIll58GT2gyoXVgZ+HsP9Dw5deesdJ+0/hfYe/NxevAIR+H9CC+XFuwcpM0wZVRWEV3y6LWS4hZhI7AQrqVonZYeFdaPBdnXLdgwGl2+LH6lg08z+yKItOMQ2AjVVxpgMX/DNMPS5KXMj/hWvmHgtEInXrsBthk/iTDiMpHeT8Yv184Q9qf8yGpPipXLTA3mazYnVtZIrMhb3ISihDmblZF8+aZpibRp1N48Wo7+SPdoZJ+S2PYW2GVAhgdoIwzTYxARGh49PkYh4SLFJbVp9mQmWkKMil4CQQFfF9KSlM5mrEcZSDJuSQpA7TLMDQQ0uboxkSqMYjUlsQ== SAMSUNG@Swamy"
-  #public_key = file("~/.ssh/tools.pub")
-  # ~ means windows home directory
-}
+
 
 module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -116,4 +120,14 @@ resource "aws_security_group_rule" "allow_nexus_8081" {
   cidr_blocks              = ["0.0.0.0/0"] # Or restrict as needed
   security_group_id        = "sg-03931b1bad72d1a95"
   description              = "Allow access to Nexus UI on port 8081"
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Or restrict to your IP for better security
+  security_group_id = "sg-03931b1bad72d1a95"
+  description       = "Allow SSH access"
 }
